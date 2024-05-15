e447d07a046e8d822d5df312db262a8141f4fbf9
Jan Singon Slany <singond@seznam.cz>
2024-05-16 00:59:51 +0200

    Plot many eigenvectors

--- schrodinger.nb.jl
+++ schrodinger.nb.jl
@@ -289,13 +289,24 @@ Vlastní vektory hamiltoniánu spočtené zvolenou metodou:
 # ╔═╡ a3b74a06-c25e-4ecc-bfb1-c669e735809b
 eg = eigenvectors(Hg, 800)
 
+# ╔═╡ 367b7aac-5086-4108-9e3a-9dff1861aa15
+erg = reshape(eg, N, N, :)
+
 # ╔═╡ 3c8b44de-2986-4cbb-aca3-fd88b78dee6b
 @bind kg html"k = <input type='number' value='1' min='1' max='800'>"
 
 # ╔═╡ 11a63b16-1f0d-49d4-8a15-0cb726e2627a
 with(ratio=1, title="vlastní vektor $(kg)") do
-	heatmap(x, y, reshape(eg[:,kg], N, N),
+	heatmap(x, y, erg[:,:,kg],
 		xlabel="x [nm]", ylabel="y [nm]")
+end
+
+# ╔═╡ 415d1921-b87d-40ec-90e7-32e040a1ac83
+with(ratio=1, size=(600,2500), plot_title="", legend=false, showaxis=false) do
+	plotsb = map(1:48) do k
+		heatmap(x, y, erg[:,:,k], title="$k")
+	end
+	plot(plotsb..., layout=(12,4))
 end
 
 # ╔═╡ b14e5668-fbc6-4a8b-b613-c37335abfa77
@@ -350,13 +361,24 @@ Vlastní vektory hamiltoniánu spočtené zvolenou metodou:
 # ╔═╡ 952eeef1-f3bc-48bd-a974-21d4dea587aa
 eb = eigenvectors(Hb, 800)
 
+# ╔═╡ dcf97088-5af4-4ed9-b3d7-4a8794e9277b
+erb = reshape(eb, N, N, :)
+
 # ╔═╡ 6f7138ab-133a-4e2a-9a56-87754b368450
 @bind kb html"<input type='number' value='1' min='1' max='800'>"
 
 # ╔═╡ 16e7014f-d553-4a08-9dec-0e5f33946c3f
 with(ratio=1, title="vlastní vektor $(kb)") do
-	heatmap(x, y, reshape(eb[:,kb], N, N),
+	heatmap(x, y, erb[:,:,kb],
 		xlabel="x [nm]", ylabel="y [nm]")
+end
+
+# ╔═╡ f2d7ee1f-05a8-48ff-ab9e-572e21b43990
+with(ratio=1, size=(600,2500), plot_title="", legend=false, showaxis=false) do
+	plotsb = map(1:48) do k
+		heatmap(x, y, erb[:,:,k], title="$k")
+	end
+	plot(plotsb..., layout=(12,4))
 end
 
 # ╔═╡ Cell order:
@@ -398,8 +420,10 @@ end
 # ╠═026934ec-6f67-45f4-bfec-fc44ab6fa913
 # ╟─648af4da-437f-4207-bb63-200b29ff1243
 # ╠═a3b74a06-c25e-4ecc-bfb1-c669e735809b
+# ╠═367b7aac-5086-4108-9e3a-9dff1861aa15
 # ╟─3c8b44de-2986-4cbb-aca3-fd88b78dee6b
 # ╠═11a63b16-1f0d-49d4-8a15-0cb726e2627a
+# ╠═415d1921-b87d-40ec-90e7-32e040a1ac83
 # ╟─b14e5668-fbc6-4a8b-b613-c37335abfa77
 # ╠═b206e6a2-6b00-4863-aecb-833f17e9fc77
 # ╠═5d3807b6-fdde-4758-8955-77816f2758c5
@@ -410,5 +434,7 @@ end
 # ╠═6579d3bb-bf7e-46c2-ae8b-11627be11b11
 # ╟─e59cfa75-c127-4fde-bea2-af97f2857dcd
 # ╠═952eeef1-f3bc-48bd-a974-21d4dea587aa
+# ╠═dcf97088-5af4-4ed9-b3d7-4a8794e9277b
 # ╟─6f7138ab-133a-4e2a-9a56-87754b368450
 # ╠═16e7014f-d553-4a08-9dec-0e5f33946c3f
+# ╠═f2d7ee1f-05a8-48ff-ab9e-572e21b43990
